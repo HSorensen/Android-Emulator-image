36bc11852a8d0e9136ace12540c0224e355fd3ca
hsorensen <hsorensen@rodanandfields.com>
2023-12-27 09:50:38 -0800

    runit.sh: added

---
+++ hsutils/runit.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+docker build --format docker -t android-emulator .
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "Error building image: $retVal"
+    return $retVal
+fi
+
+
+docker run --rm -it -d -p 5900:5900 --name androidContainer -e VNC_PASSWORD=password --privileged android-emulator
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "Error running image: $retVal"
+    return $retVal
+fi
+
+# get container id
+CONTAINERID=`docker container ls --all | grep androidContainer | cut -d" " -f1`
+
+echo "Started container $CONTAINERID"
+
+docker exec --privileged -it androidContainer bash -c "./start_vnc.sh"
+
+echo "Stopping container $CONTAINERID ... "
+
+docker stop $CONTAINERID
+
+echo "Container stopped."
